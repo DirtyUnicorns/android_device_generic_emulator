330229a1c88287c9d64ce52bd622c561cbf90687
maxwen <max.weninger@gmail.com>
2019-09-07 00:57:51 +0200

    emulator: make emulator work on Q
    Change-Id: I4fe6e0ed2604acdec56bc1851f5053e2373bbfc5

--- emulator/start_emulator_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# assumes the image zip has been extracted in /tmp
-
-~/Android/Sdk/emulator/emulator -verbose -skindir $ANDROID_BUILD_TOP/vendor/extras/emulator/skins/ -skin pixel_2_xl -writable-system -gpu host -wipe-data
