55802262cade4a579e711a86108696422252bd41
Nicolas Dhouailly <ndhouailly@gmail.com>
2019-12-14 14:21:39 +0100

    emulator: create zip in ANDROID_PRODUCT_OUT directory

--- create_emulator_image.sh
+++ create_emulator_image.sh
@@ -15,7 +15,7 @@ if [ ! -f $ANDROID_PRODUCT_OUT/system.img ]; then
     exit 1
 fi
 
-# creates image zip in /tmp/
+# creates image zip in $ANDROID_PRODUCT_OUT
 cd $ANDROID_PRODUCT_OUT/..
 DIGEST=`grep "# " generic_x86/VerifiedBootParams.textproto| sed -e 's/# //'`
 
@@ -26,5 +26,7 @@ chmod 755 $ANDROID_PRODUCT_OUT/start_emulator_image.sh
 cp -r device/generic/emulator/advancedFeatures.ini $ANDROID_PRODUCT_OUT
 
 cd $ANDROID_PRODUCT_OUT/..
-rm /tmp/du_emulator.zip
-zip -r /tmp/du_emulator.zip generic_x86/skins generic_x86/system-qemu.img generic_x86/system/build.prop generic_x86/cache.img generic_x86/userdata.img generic_x86/start_emulator_image.sh generic_x86/advancedFeatures.ini generic_x86/encryptionkey.img generic_x86/kernel-ranchu-64 generic_x86/ramdisk.img generic_x86/vendor-qemu.img
+if [ -e $ANDROID_PRODUCT_OUT/du_emulator.zip ]; then
+    rm $ANDROID_PRODUCT_OUT/du_emulator.zip
+fi
+zip -r $ANDROID_PRODUCT_OUT/du_emulator.zip generic_x86/skins generic_x86/system-qemu.img generic_x86/system/build.prop generic_x86/cache.img generic_x86/userdata.img generic_x86/start_emulator_image.sh generic_x86/advancedFeatures.ini generic_x86/encryptionkey.img generic_x86/kernel-ranchu-64 generic_x86/ramdisk.img generic_x86/vendor-qemu.img
